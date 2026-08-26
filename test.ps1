f511e5638aa5b22dc8f2690b4491e8e88c5c124b
piotrekmarcelpiotrekmarcel <piotrek.admin.produban.co.uk.marcel@gmail.com>
2026-08-26 12:34:35 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,8 @@
-whoami
 whoami /all
 hostname
+systeminfo | findstr /B /C:"OS Name" /C:"OS Version"
+systeminfo | findstr /i "domain"
+[Environment]::UserDomainName
+[Environment]::Is64BitProcess
+(Get-WmiObject Win32_ComputerSystem).Domain
+ipconfig /all
