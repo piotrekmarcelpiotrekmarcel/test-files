e435e63c392c60f70a02bf72df6d2eb300383365
piotrekmarcelpiotrekmarcel <piotrek.admin.produban.co.uk.marcel@gmail.com>
2026-08-26 13:22:32 +0200

    Delete files directory

--- files/c.ps1
+++
@@ -1,3 +0,0 @@
-# Staged payload for T1059 test #16 (download cradle, WebClient held in variable)
-"$(Get-Date -Format o) | c.ps1 executed on $(hostname) as $(whoami)" | Out-File -Append C:\purpletest\evidence\cradle_c.log
-whoami /all
--- files/s.ps1
+++
@@ -1,3 +0,0 @@
-# Staged payload for T1059 test #27 (HTTPS download cradle + TLS cert validation bypass)
-"$(Get-Date -Format o) | s.ps1 executed on $(hostname) as $(whoami)" | Out-File -Append C:\purpletest\evidence\cradle_s.log
-ipconfig /all
